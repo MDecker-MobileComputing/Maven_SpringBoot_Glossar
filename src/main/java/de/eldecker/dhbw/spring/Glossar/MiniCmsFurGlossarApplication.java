0091a05446c612058156b2891f265e6a2405a311
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-09 12:04:26 +0200

    Projekt erzeugt mit Spring Initializr: bit.ly/springboot_glossar

---
+++ src/main/java/de/eldecker/dhbw/spring/Glossar/MiniCmsFurGlossarApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.Glossar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCmsFurGlossarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCmsFurGlossarApplication.class, args);
+	}
+
+}
