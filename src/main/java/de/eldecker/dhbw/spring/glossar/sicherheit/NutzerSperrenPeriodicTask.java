e55779d6c0a1dc8e2fef9c8a3902b7259b7b9bf9
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-12 10:36:17 +0200

    Zeitspanne fuer Sperren inaktiver Nutzer kann jetzt in application.properties konfiguriert werden

--- src/main/java/de/eldecker/dhbw/spring/glossar/sicherheit/NutzerSperrenPeriodicTask.java
+++ src/main/java/de/eldecker/dhbw/spring/glossar/sicherheit/NutzerSperrenPeriodicTask.java
@@ -1,13 +1,19 @@
 package de.eldecker.dhbw.spring.glossar.sicherheit;
 
+import static java.lang.String.format;
+
 import de.eldecker.dhbw.spring.glossar.db.Datenbank;
 import de.eldecker.dhbw.spring.glossar.db.entities.AutorEntity;
 
+import jakarta.transaction.Transactional;
+
 import java.util.List;
 
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import org.springframework.scheduling.annotation.Scheduled;
 
 import org.slf4j.Logger;
@@ -26,8 +32,16 @@ public class NutzerSperrenPeriodicTask {
 
     private final static Logger LOG = LoggerFactory.getLogger( NutzerSperrenPeriodicTask.class );
 
+    /**
+     * Konfiguration aus {@code application.properties}: Anzahl Minuten nach letztem Login,
+     * nachdem ein Nutzer gesperrt wird.
+     */
+    @Value( "${de.eldecker.glossar.inaktivitaet.minuten:99999}" )
+    private int _konfigurationMinutenInaktivitaet;
+
     /** Bean für Datenbankzugriff. */
     private Datenbank _datenbank;
+
 
     /**
      * Konstruktor für <i>Dependency Injection</i>.
@@ -35,6 +49,21 @@ public class NutzerSperrenPeriodicTask {
     public NutzerSperrenPeriodicTask( Datenbank datenbank ) {
 
         _datenbank = datenbank;
+    }
+
+
+    /**
+     * Da Konfigurationen erst nach der Ausführung des Konstruktors verfügbar sind
+     * wird der konfigurierte Wert in dieser mit {@code PostConstruct} annotierten
+     * Methode geloggt.
+     */
+    @PostConstruct
+    private void loggeKonfiguration() {
+
+        final String anzahlMinutenFormatiert = format( "%,d", _konfigurationMinutenInaktivitaet );
+
+        LOG.info( "Zeitspanne nach letztem Login, nachdem ein Nutzer gesperrt wird: {} Minuten",
+                  anzahlMinutenFormatiert );
     }
 
 
@@ -48,12 +77,23 @@ public class NutzerSperrenPeriodicTask {
      * wäre dies ein Zeitraum von mehreren Wochen oder Monaten).
      */
     @Scheduled(fixedRate = 3*60*1000)
+    @Transactional
     public void nutzerSperren() {
 
-        final List<AutorEntity> inaktivenAutorenList = _datenbank.getInaktiveAutoren( 3 );
+        final List<AutorEntity> inaktivenAutorenList =
+                                    _datenbank.getInaktiveAutoren( _konfigurationMinutenInaktivitaet );
 
         LOG.info( "Anzahl Autoren/Nutzer gefunden, die lange nicht mehr angemeldet waren: {}",
                   inaktivenAutorenList.size() );
+
+        inaktivenAutorenList.forEach( autor -> {
+
+            autor.setIstAktiv( false );
+            _datenbank.updateAutor( autor );
+
+            LOG.info( "Autor/Nutzer \"{}\" wird gesperrt wegen langer Inaktivität.",
+                      autor.getNutzername() );
+        });
     }
 
 }
